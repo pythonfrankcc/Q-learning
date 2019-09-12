a6401e0b9aa35b3df99f9f8e6131366166e829d9
pythonfrankcc <francismurithi475@gmail.com>
2019-09-12 10:11:13 +0300

    added regularizer to second layer

--- deep_learning_keras.py
+++ deep_learning_keras.py
@@ -10,15 +10,17 @@ from keras.layers.core import Dense
 from keras.layers import Activation
 from keras.optimizers import Adam
 from keras.metrics import categorical_cross entropy 
+from keras import regularizers#allows you to add penalty and are applied on a per layer basis
 
 #creating a variable called model and setting it to an instance of a sequential object
 model = Sequential(layers)
 
 #dense layers are the regular hidden layers found between the input and output
+#a higher value for a regularizer means less variance for the model and thus less complexity
 
 layers = [
      Dense(16, input_shape = (1,), activation = 'relu'),#input layer with one nodes and the dense layer with 16 nodes
-     Dense(32, activation = 'relu'),#hidden layer with 32 nodes
+     Dense(32, activation = 'relu', kernel_regularizer = regularizers.l2(0.01)),#hidden layer with 32 nodes with a regularizer to reduce overfitting
      Dense(2, activation = 'sigmoid')#creating the output layer with only two nodes
 ]
 
